38e21154c2c9c91c55bf5ce48c0a99d995328116
vikash <vikashpandey0018@gmail.com>
2020-01-23 19:33:56 +0530

    initial commit main file changed

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Calculator'),
+      home: new MyHomePage(title: 'Calcula'),
     );
   }
 }
